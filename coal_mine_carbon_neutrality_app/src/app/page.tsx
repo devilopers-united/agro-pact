18ca251806a3e4aebe0e539af7f6252ad20584b3
SHORYA BAJ <jain.shorya11@gmail.com>
2024-09-12 02:13:31 +0530

    Fix: Changed the ui of navbar and hero

--- coal_mine_carbon_neutrality_app/src/app/page.tsx
+++ coal_mine_carbon_neutrality_app/src/app/page.tsx
@@ -3,41 +3,12 @@ import { useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  const handleNavigation = (path: string) => {
-    if (session) {
-      router.push(path);
-    } else {
-      router.push("/signin");
-    }
-  };
-
   return (
-    <main className="flex min-h-screen flex-col items-center py-8 px-24 relative justify-between overflow-hidden sm:px-10">
-      <div className="flex flex-col items-center">
-        <h1 className="text-6xl font-bold">AgriBazar</h1>
-        <p className="text-2xl text-center">
-          Connecting consumers and producers via a marketplace
-        </p>
-      </div>
-      <div className="absolute right-0 top-1/2 transform -translate-y-1/2 flex flex-col space-y-6">
-        <Button
-          className="bg-slate-500 text-xl py-4 px-8"
-          onClick={() => handleNavigation("/dashboard/farmer")}
-        >
-          Farmer
-        </Button>
-        <Button
-          className="bg-slate-500 text-xl py-4 px-8"
-          onClick={() => handleNavigation("/dashboard/buyer")}
-        >
-          Buyer
-        </Button>
-      </div>
+    <main className="flex min-h-screen  py-8 px-24 relative justify-between overflow-hidden sm:px-10">
+      <Hero />
     </main>
   );
 }
--- coal_mine_carbon_neutrality_app/src/components/Hero.tsx
+++ coal_mine_carbon_neutrality_app/src/components/Hero.tsx
@@ -1,7 +1,26 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import ClientNavbar from "./clientNav";
+import { Typewriter } from "./TypeWriter";
 
 const Hero = () => {
-  return <div>Hello</div>;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const handleNavigation = (path: string) => {
+    if (session) {
+      router.push(path);
+    } else {
+      router.push("/signin");
+    }
+  };
+  return (
+    <div>
+      <Typewriter />
+    </div>
+  );
 };
 
 export default Hero;
